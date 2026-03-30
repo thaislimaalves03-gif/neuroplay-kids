c7dc1651c3b7bc11e3752a808b1dfddad3421e94
thaislimaalves03-gif <thaislimaalves03@gmail.com>
2026-03-30 00:25:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+const adminEmail = "thaislimaalves03@gmail.com";
 
+// LOGIN SIMPLES
+function login() {
+  let email = prompt("Digite seu Gmail:");
+  let user = { email: email };
+
+  verificarAdmin(user);
+}
+
+// VERIFICA ADMIN
+function verificarAdmin(user) {
+  if (user.email === adminEmail) {
+    document.getElementById("adminPanel").style.display = "block";
+    alert("Você é ADMIN 😎");
+  } else {
+    alert("Bem-vindo!");
+  }
+}
+
+// ADICIONAR FILME
+function adicionarFilme() {
+  let titulo = document.getElementById("titulo").value;
+  let url = document.getElementById("url").value;
+
+  let card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `
+    <h3>${titulo}</h3>
+    <video src="${url}" controls width="300"></video>
+  `;
+
+  document.querySelector(".filmes").appendChild(card);
+}
+
+// MUDAR COR
+function mudarCor() {
+  let cor = document.getElementById("corSite").value;
+  document.body.style.backgroundColor = cor;
+}
